244974895d13719979565df3fb831249f6e71787
Mariam Zareen <119798530+MariamZareen@users.noreply.github.com>
2024-07-19 10:28:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.post ('/api/create-checkout-session',async(req,res)=>{
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-     success_url: "http://localhost:3000/",
-     cancel_url: "http://localhost:3000/cancel"
+     success_url: "https://movie-ticket-booking-system-frontend.vercel.app/",
+     cancel_url: "https://movie-ticket-booking-system-frontend.vercel.app/cancel"
    })
    try {
     const user = new bookedData({
